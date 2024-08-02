03ba188182893eeed2790a9fd62c0ba698111894
Javier Clavijo <jjclavijo@gmail.com>
2024-08-02 18:43:32 -0300

    intento samplear -> me duermo

--- tests/una_placa.rs
+++ tests/una_placa.rs
@@ -1,12 +1,27 @@
 use mobius_project::mobius::MobiusTransformation;
 use mobius_project::geo::GeographicPoint;
 
+
+use rand::Rng;
+use rand::distributions::Uniform;
+use std::f64::consts::PI; 
+
+fn sample_rectangulo<R:Rng + ?Sized>(r: &mut R,l: f64, a: f64, b: f64, c: f64, d:f64) -> Vec<GeographicPoint>
+    {
+        let x = Uniform::new(a,b);
+        let y = Uniform::new(c,d);
+        
+        vec![GeographicPoint::new(x.sample(r),y.sample(r))]
+
+    }
+
 #[test]
 fn test_placa() {
 
     // Simulación de una placa
 
     let transform = MobiusTransformation::from_pole(30.0,40.0,0.01);  
+
 
     // Se generan puntos
     let puntos: Vec<GeographicPoint> = vec![GeographicPoint::new(31.0,42.0)];
